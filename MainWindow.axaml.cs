138558471d05d1d9f7419a461ce0f443dba6d17b
Neil Roodyn <Neil@roodyn.com>
2022-03-05 12:51:22 +1100

    Create new app

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace dotnetPiPictureFrame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
