564bf801422c59f9acd999f34f3f342728fa4867
Neil Roodyn <Neil@roodyn.com>
2022-03-05 13:11:02 +1100

    Add FrameViewModel

---
+++ FrameViewModel.cs
@@ -0,0 +1,28 @@
+using System;
+using ReactiveUI;
+using Weather.NET.Models.WeatherModel;
+
+public class FrameViewModel : ReactiveObject
+{
+    string? photoPath;
+    public string? PhotoPath 
+    { 
+        get => photoPath; 
+        set => this.RaiseAndSetIfChanged(ref photoPath, value); 
+    }
+
+    DateTime time;
+    public DateTime Time 
+    { 
+            get => time;
+            set => this.RaiseAndSetIfChanged(ref time, value);  
+    }
+
+    WeatherModel? currentWeather;
+
+    public WeatherModel? CurrentWeather
+    {
+        get => currentWeather;
+        set => this.RaiseAndSetIfChanged(ref currentWeather, value);  
+    }
+}
