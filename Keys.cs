82c2dbaba8c11913be76a211883110765be937db
Neil Roodyn <Neil@roodyn.com>
2022-03-05 13:46:20 +1100

    Add WeatherClient to mainwindow Add city to keys

--- Keys.cs
+++ Keys.cs
@@ -1,4 +1,5 @@
 internal static class Keys
 {
     internal const string OpenWeather = "your key goes here";
+    internal const string WeatherCity = "Sydney, NSW";
 }
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,10 +1,13 @@
 using Avalonia.Controls;
+using Weather.NET;
 
 namespace dotnetPiPictureFrame
 {
     public partial class MainWindow : Window
     {
         FrameViewModel viewModel = new FrameViewModel();
+        WeatherClient client = new WeatherClient(Keys.OpenWeather);
+        
         public MainWindow()
         {
             viewModel.Time = System.DateTime.Now;
