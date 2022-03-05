c25aaee27e6c9c71fb556056d1653b6a79dfb48a
Neil Roodyn <Neil@roodyn.com>
2022-03-05 13:58:47 +1100

    Add UpdateGui loop move photos path to Keys

--- Keys.cs
+++ Keys.cs
@@ -1,5 +1,6 @@
 internal static class Keys
 {
-    internal const string OpenWeather = "your key goes here";
+    internal const string OpenWeather = "<your open weather key goes here>";
     internal const string WeatherCity = "Sydney, NSW";
+    internal const string PhotosFolder = @"<path to where you store photos>";
 }
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using System;
+using System.Threading.Tasks;
 using Weather.NET;
 using Weather.NET.Enums;
 
@@ -8,14 +10,32 @@ namespace dotnetPiPictureFrame
     {
         FrameViewModel viewModel = new FrameViewModel();
         WeatherClient client = new WeatherClient(Keys.OpenWeather);
-        
+        const int checkWeatherPeriod = 60;
+        int currentPeriodSeconds = 0;
+
         public MainWindow()
         {
-            viewModel.Time = System.DateTime.Now;
-            viewModel.CurrentWeather = client.GetCurrentWeather(cityName: Keys.WeatherCity, measurement: Measurement.Metric);
-            viewModel.PhotoPath = @"\\Creative\Photos\PicFrame1\LachSonnKrisNeil.png";
+            viewModel.PhotoPath = Keys.PhotosFolder;
             InitializeComponent();
             DataContext = viewModel;
+            Task.Run(async () => await UpdateGUI());
+        }
+
+        private async Task UpdateGUI()
+        {
+            viewModel.CurrentWeather = client.GetCurrentWeather(cityName: Keys.WeatherCity, measurement: Measurement.Metric);
+            while (true)
+            {
+                viewModel.Time = DateTime.Now;
+                
+                if (currentPeriodSeconds > checkWeatherPeriod)
+                {
+                    viewModel.CurrentWeather = client.GetCurrentWeather(cityName: Keys.WeatherCity, measurement: Measurement.Metric);
+                    currentPeriodSeconds = 0;
+                }
+                currentPeriodSeconds++;
+                await Task.Delay(1000);
+            }
         }
     }
 }
