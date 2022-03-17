2c9ff7c69ace1754fd8187b2ab074df590447d68
Neil Roodyn <Neil@roodyn.com>
2022-03-17 19:56:06 +1100

    Add cam frame view image to the window

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
 using Avalonia.Threading;
 using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Weather.NET;
 using Weather.NET.Enums;
@@ -18,13 +20,17 @@ namespace dotnetPiPictureFrame
         int updatePhotoPeriod = 1;
         int photoPeriodSeconds = 0;
         Image? photoImage;
-
+        string VideoUrl => @"http://yellowpi:8080/Video";
+        Image frameImage;
+        Bitmap? frameBmp;
         public MainWindow()
         {
             InitializeComponent();
             photoImage = this.FindControl<Image>("PhotoImage");
+            frameImage = this.FindControl<Image>("FrameImage");
             DataContext = viewModel;
             Task.Run(async () => await UpdateGUI());
+            Task.Run(async () => await UpdateFrameImage());
         }
 
         async Task UpdateGUI()
@@ -73,5 +79,63 @@ namespace dotnetPiPictureFrame
                 await Task.Delay(1000);
             }
         }
+
+        async Task UpdateFrameImage()
+        {
+            if (frameImage != null)
+            {
+                HttpClient client = new HttpClient();
+
+                using HttpResponseMessage response = await client.GetAsync(VideoUrl, HttpCompletionOption.ResponseHeadersRead);
+
+                using HttpContent content = response.Content;
+                using var stream = await content.ReadAsStreamAsync();
+
+                byte[] buffer = new byte[4096];
+                var lengthMarker = "Content-Length:";
+                var endMarker = "\r\n\r\n";
+
+                while (true)
+                {
+                    try
+                    {
+                        Array.Fill<byte>(buffer, 0, 0, buffer.Length);
+                        int len = await stream.ReadAsync(buffer, 0, buffer.Length);
+                        var header = System.Text.Encoding.Default.GetString(buffer);
+
+                        var lengthStart = header.IndexOf(lengthMarker) + lengthMarker.Length;
+                        var lengthEnd = header.IndexOf(endMarker);
+                        if (lengthEnd > lengthStart)
+                        {
+                            var lengthString = header.Substring(lengthStart, lengthEnd - lengthStart);
+
+                            int frameSize = int.Parse(lengthString);
+                            byte[] frameBuffer = new byte[frameSize];
+
+                            int totalBytesCopied = (int)len - (lengthEnd + endMarker.Length);
+                            if (totalBytesCopied > 0)
+                            {
+                                Array.Copy(buffer, lengthEnd + endMarker.Length, frameBuffer, 0, totalBytesCopied);
+                            }
+
+                            while (totalBytesCopied < frameSize)
+                            {
+                                totalBytesCopied += await stream.ReadAsync(frameBuffer, totalBytesCopied, frameBuffer.Length - totalBytesCopied);
+                                await Task.Yield();
+                            }
+                            using MemoryStream ms = new(frameBuffer);
+                            frameBmp = new Bitmap(ms);
+
+                            await Dispatcher.UIThread.InvokeAsync(() => { frameImage.Source = frameBmp; });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+        }
+
     }
 }
