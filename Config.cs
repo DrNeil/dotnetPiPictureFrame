22fd383a98cadc53894c42a74465dcf0faeb7dc8
Neil Roodyn <Neil@roodyn.com>
2022-03-18 17:42:32 +1100

    Add VideoURL to config

--- Config.cs
+++ Config.cs
@@ -3,4 +3,5 @@ internal static class Config
     internal const string OpenWeather = "<your open weather key goes here>";
     internal const string WeatherCity = "Sydney, NSW";
     internal const string PhotosFolder = @"<path to where you store photos>";
+    internal const string VideoUrl = @"http://raspberrypi:8080/Video";
 }
