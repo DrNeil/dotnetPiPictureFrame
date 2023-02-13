4a74fa01d874640d3d2c2186fb119753db972ef6
Neil Roodyn <Neil@roodyn.com>
2023-02-13 19:51:28 +1100

    Display bins for collection

--- BinCollectionConverter.cs
+++ BinCollectionConverter.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Globalization;
+using Avalonia;
 using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace dotnetPiPictureFrame
 {
@@ -15,11 +18,62 @@ namespace dotnetPiPictureFrame
             {
                 if (binCollection.Bins != null)
                 {
-                    return $"In {binCollection.DaysToNextCollection} day(s), {binCollection.Bins}";
+                    if (binCollection.DaysToNextCollection == 0)
+                    {
+                        return "Today";
+                    }
+                    else if (binCollection.DaysToNextCollection == 1)
+                    {
+                        return "Tomorrow";
+                    }
+                    else if (binCollection.DaysToNextCollection > 1)
+                    {
+                        return $"In {binCollection.DaysToNextCollection} days";
+                    }
+                    else
+                    {
+                        return "Cannot find bins to collect";
+                    }
+                    
                 }
                 else
                 {
                     return "Cannot find bins to collect";
+                }
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class BinImageConverter: IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return null;
+            var binString = parameter.ToString();
+            if (binString == null)
+                return null;
+
+            if (value is Bins.BinCollection binCollection)
+            {
+                var binNumber = int.Parse(binString);
+                if (binCollection.Bins != null
+                    && binCollection.Bins.Length > binNumber)
+                {
+                    var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+                    var bitmap = new Bitmap(assets?.Open(new Uri($"avares://dotnetPiPictureFrame/Assets/{binCollection.Bins[binNumber]}.png")));
+                    return bitmap;//new Bitmap(filepath);
+                }
+                else
+                {
+                    return null;
                 }
             }
 
